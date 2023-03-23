3d59de7a5cea176b873b85216979cc071e0908e4
Alex Brian Naughton <alexbriannaughton@gmail.com>
2023-03-22 18:49:50 -0700

    search filter practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const list = ['Tummy Rooters', 'Jeans Baltar Lil Boo Hammond', 'Lucy Liu', 'Sagan Mccrorey', 'Gustard']
+
+  const [filterList, setFilterList] = useState(list)
+
+  const handleSearch = (e) => {
+    const filteredValues = list.filter(
+      (item) => {
+        return item.toLowerCase().includes(e.target.value)
+    })
+    setFilterList(filteredValues)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div id='search-div'>
+        {'lookie=>'} <input name='query' type="text" onChange={handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((pet) => (
+          <h2 key={pet}>{pet}</h2>
+        ))}
     </div>
   );
 }
